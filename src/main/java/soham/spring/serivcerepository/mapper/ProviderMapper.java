1a2c8cfadbe9f9b41dce2dd4b8de383518b62efa
Soham <iam.soham@gmail.com>
2021-03-05 14:42:31 +0100

    provider services api

--- src/main/java/soham/spring/serivcerepository/mapper/ProviderMapper.java
+++ src/main/java/soham/spring/serivcerepository/mapper/ProviderMapper.java
@@ -3,6 +3,7 @@ package soham.spring.serivcerepository.mapper;
 import org.mapstruct.Mapper;
 import soham.spring.serivcerepository.entity.ProviderEntity;
 import soham.spring.serivcerepository.model.Provider;
+import soham.spring.serivcerepository.model.ProviderServices;
 
 import java.util.List;
 
@@ -14,4 +15,6 @@ public interface ProviderMapper {
     List<Provider> toProviders(List<ProviderEntity> providerEntities);
 
     ProviderEntity toEntity(Provider provider);
+
+    ProviderServices toProviderServices(ProviderEntity providerEntity);
 }
--- src/main/java/soham/spring/serivcerepository/repository/ServiceRepository.java
+++ src/main/java/soham/spring/serivcerepository/repository/ServiceRepository.java
@@ -1,7 +1,12 @@
 package soham.spring.serivcerepository.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import soham.spring.serivcerepository.entity.ProviderEntity;
 import soham.spring.serivcerepository.entity.ServiceEntity;
 
+import java.util.List;
+
 public interface ServiceRepository extends JpaRepository<ServiceEntity, Integer> {
+
+    public List<ServiceEntity> findAllByProviderEntity(ProviderEntity providerEntity);
 }
